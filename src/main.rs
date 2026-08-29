42e25542fab4e7576004d5602b4aa599f8bd094a
ayxan <ayxan.20145@gmail.com>
2026-08-29 23:53:59 +0400

    added a print function

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,22 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    unsafe {
-        *vga_buffer.add(0) = b'h';
-        *vga_buffer.add(1) = 0x0F;
-        *vga_buffer.add(2) = b'i';
-        *vga_buffer.add(3) = 0x0F;
-    }
+    let text = "hello world";
+    let fg = 0x04;
+    print(text, fg);
 
     loop {}
+}
+
+fn print(text: &str, fg: u8) {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, byte) in text.bytes().enumerate() {
+        unsafe {
+            *vga_buffer.add(i * 2) = byte;
+            *vga_buffer.add(i * 2 + 1) = fg;
+        }
+    }
 }
 
 #[panic_handler]

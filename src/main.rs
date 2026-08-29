bfa78278db856855979ecd819e73cce81c33266f
ayxan <ayxan.20145@gmail.com>
2026-08-29 23:47:03 +0400

    display hi to screen with white foreground

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    unsafe {
+        *vga_buffer.add(0) = b'h';
+        *vga_buffer.add(1) = 0x0F;
+        *vga_buffer.add(2) = b'i';
+        *vga_buffer.add(3) = 0x0F;
+    }
+
     loop {}
 }
 

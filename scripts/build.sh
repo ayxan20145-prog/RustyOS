8d48c457274ab5eaa916ce9a14a879c45bb10465
ayxan <ayxan.20145@gmail.com>
2026-09-01 22:55:57 +0400

    remove scripts and use cargo-make instead

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cargo build
-mkdir -p iso/boot/grub
-cp grub.cfg iso/boot/grub
-cp target/i686-unknown-none/debug/RS-DOS iso/boot/rs-dos
-grub-mkrescue -o rs-dos.iso iso
--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cargo clean
-rm -rf iso rs-dos.iso
